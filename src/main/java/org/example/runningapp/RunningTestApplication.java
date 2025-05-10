9b0b92d2bb96aaba77b1b649540af907006827a1
lsheon0927@naver.com <lsheon0927@naver.com>
2025-05-10 22:03:42 +0900

    init: 테스트 환경 가져오기

---
+++ src/main/java/org/example/runningapp/RunningTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.runningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningTestApplication.class, args);
+	}
+
+}
